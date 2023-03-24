953101f94e3fb34821c410cdb4a63f6ddad58290
Lucas Ramos <lucasramos1123.lr29@gmail.com>
2023-03-24 08:38:21 -0300

    add: new component lancamentos-pesquisa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,4 @@ export class AppComponent {
 
   
 
-  lancamentos = [
-    {tipo: 'DESPESA', descricao: 'Compra de Pães', dataVencimento: '30/12/2022', dataPagamento: null, valor: 4.45, pessoa: 'Padaria do Jose'},
-    {tipo: 'RECEITA', descricao: 'Venda de Software', dataVencimento: '28/02/2023', dataPagamento: '26/02/2023', valor: 8000, pessoa: 'Data sotware'},
-    {tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '21/01/2022', dataPagamento: null, valor: 143125, pessoa: 'Ministerio da Fazenda'},
-    {tipo: 'DESPESA', descricao: 'Mensalidade da Escola', dataVencimento: '05/02/2023', dataPagamento: '12/04/2023', valor: 800, pessoa: 'Escolha Magalhães'},
-    {tipo: 'RECEITA', descricao: 'Venda de Carro', dataVencimento: '16/04/2022', dataPagamento: null, valor: 55000, pessoa: 'Lucas Ramos'},
-    {tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '12/03/2022', dataPagamento: '11/03/2022', valor: 1750, pessoa: 'Casa Nova Imoveis'},
-    {tipo: 'DESPESA', descricao: 'Academia', dataVencimento: '05/04/2022', dataPagamento: null, valor: 150, pessoa: 'Academia Top'}
-  ] ; 
-
-  faCoffee = faCoffee;
-  faPen = faPen;
-  faTrash = faTrash;
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TabViewModule } from 'primeng/tabview';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LacamentosPesquisaComponent } from './lacamentos-pesquisa/lacamentos-pesquisa.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LacamentosPesquisaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
