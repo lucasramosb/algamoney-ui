c28487b33b31ecd18929a46ceadb1b93b24f97dd
Lucas Ramos <lucasramos1123.lr29@gmail.com>
2023-04-04 08:19:29 -0300

    add: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LacamentosPesquisaComponent } from './lacamentos-pesquisa/lacamentos-pesquisa.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LacamentosCadastroComponent } from './lacamentos-cadastro/lacamentos-cadastro.component';
+import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'lancamentos-pesquisa', pathMatch: 'full'},
+  {path: 'lancamentos-pesquisa', component: LacamentosPesquisaComponent},
+  {path: 'pessoas-pesquisa', component: PessoasPesquisaComponent },
+  {path: 'lancamentos-cadastro', component: LacamentosCadastroComponent},
+  {path: 'pessoas-cadastro', component: PessoasCadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
