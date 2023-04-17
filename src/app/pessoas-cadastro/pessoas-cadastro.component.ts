114efa2d8ae88821394b3f2c36bfbb27d31e0e88
Lucas <lucasramos1123.lr29@gmail.com>
2023-04-17 08:48:20 -0300

    injection dependencies and method get

--- src/app/pessoas-cadastro/pessoas-cadastro.component.ts
+++ src/app/pessoas-cadastro/pessoas-cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PessoasService } from 'services/pessoas.service';
 
 @Component({
   selector: 'app-pessoas-cadastro',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./pessoas-cadastro.component.css']
 })
 export class PessoasCadastroComponent {
+
+  constructor(private pessoasService: PessoasService){
+
+  }
+
+  adicionar(nome: string, cidade: string, estado: string){
+    this.pessoasService.postPessoas({nome: nome, cidade: cidade, estado: estado})
+    .then(()=> console.log(nome, cidade, estado))
+  }
 
 }
